6efe55173edb23f09760e1690788727bcfbac631
Jan Gloser <jan.renra.gloser@gmail.com>
2012-07-22 07:08:20 +0200

    Using shared libraries

---
+++ libraries/program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+  public static void Main()
+  {
+    WingMaker shanti = new WingMaker();
+    shanti.MakeWings();
+  }
+}
---
+++ libraries/wingmaker.cs
@@ -0,0 +1,10 @@
+using System;
+
+//Needs to be public, otherwise it won't be visible to the calling program
+public class WingMaker
+{
+  public void MakeWings()
+  {
+    Console.WriteLine("The time has come to lift your gaze from the fire's brightness and cast shadows of your own.");
+  }
+}
